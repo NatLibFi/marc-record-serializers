55e02352e548ab632743ee627eaacd92853bced2
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2019-10-22 13:56:59 +0300

    Update copyright year

--- src/aleph-sequential.spec.js
+++ src/aleph-sequential.spec.js
@@ -3,7 +3,7 @@
 * @licstart  The following is the entire license notice for the JavaScript code in this file.
 *
 * Copyright 2014-2017 Pasi Tuominen
-* Copyright 2018 University Of Helsinki (The National Library Of Finland)
+* Copyright 2018-2019 University Of Helsinki (The National Library Of Finland)
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 *
